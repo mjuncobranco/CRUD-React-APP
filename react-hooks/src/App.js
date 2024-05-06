4636d148a225989af4768f5fc1fc1b74b5462f43
mjuncobranco <mjuncobranco@gmail.com>
2024-05-06 18:03:41 +0200

    deleting extra files & setting App.js, index.js & index.css file

---
+++ react-hooks/src/App.js
@@ -0,0 +1,20 @@
+
+import React from "react";
+
+function App() {
+  return (
+    <div className="container">
+      <h1>CRUD React App </h1>
+      <div className="flex-row">
+        <div className="flex-large">
+          <h2>Add user</h2>
+        </div>
+        <div className="flex-large">
+          <h2>View users</h2>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
