8423b09a845b4870b52e8c124ba7a6a3db8a6d26
mjuncobranco <mjuncobranco@gmail.com>
2024-05-06 19:10:48 +0200

    UsersTable ready,still need to work on hooking the edit & delete buttons

--- react-hooks/src/tables/UserTable.js
+++ react-hooks/src/tables/UserTable.js
@@ -1,5 +1,5 @@
 // import './UserTable.css';
-import React from 'react';
+import React from "react";
 
 const UserTable = (props) => {
   return (
@@ -12,26 +12,25 @@ const UserTable = (props) => {
         </tr>
       </thead>
       <tbody>
-      {props.users.length > 0 ? (
-        props.users.map((user)=>(
-          <tr key={user.id}>
-            <td>{user.name}</td>
-            <td>{user.username}</td>
-            <td>
-            <button className="button muted-button">Edit</button>
-            <button className="button muted-button">Delete</button>
-            </td>
+        {props.users.length > 0 ? (
+          props.users.map((user) => (
+            <tr key={user.id}>
+              <td>{user.name}</td>
+              <td>{user.username}</td>
+              <td>
+                <button className="button muted-button">Edit</button>
+                <button className="button muted-button">Delete</button>
+              </td>
+            </tr>
+          ))
+        ) : (
+          <tr>
+            <td colSpan={3}>No users</td>
           </tr>
-          
-        ))
-      ) : (
-        <tr>
-          <td colSpan={3}>No users</td>
-        </tr>     
-      )}  
+        )}
       </tbody>
     </table>
   );
-}
+};
 
 export default UserTable;
