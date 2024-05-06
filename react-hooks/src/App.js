ddb09daef4d79c4726290270f44bd88648faafd2
mjuncobranco <mjuncobranco@gmail.com>
2024-05-06 18:33:19 +0200

    creating a usersData array of objects with users data (name&userName) & creating a hook of state to handle users changes

--- react-hooks/src/App.js
+++ react-hooks/src/App.js
@@ -1,8 +1,17 @@
-
-import React from "react";
+import React, { useState } from "react";
 import UserTable from "./tables/UserTable";
 
 function App() {
+
+  const usersData = [
+    { id: 1, name: "Bruce", username: "batman" },
+    { id: 2, name: "Clark", username: "superman" },
+    { id: 3, name: "Tony", username: "ironman" },
+    { id: 4, name: "Stephen", username: "strange" },
+  ];
+
+  const [users, setUsers]= useState(usersData);
+  
   return (
     <div className="container">
       <h1>CRUD React App </h1>
@@ -12,7 +21,7 @@ function App() {
         </div>
         <div className="flex-large">
           <h2>View users</h2>
-          <UserTable/>
+          <UserTable />
         </div>
       </div>
     </div>
