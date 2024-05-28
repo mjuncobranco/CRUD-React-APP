8e034aa35ae19b8e2b41d5bfd4fe19a4c74fe696
mjuncobranco <mjuncobranco@gmail.com>
2024-05-28 12:25:02 +0200

    created a Footer component & added styles

---
+++ react-hooks/src/components/footer/Footer.js
@@ -0,0 +1,14 @@
+
+import classes from '../footer/Footer.module.css';
+function Footer() {
+  return (
+    <footer className={classes.footer}>
+      <span>Designed with &#10084; by &copy;MJB {new Date().getFullYear()} </span>
+      <a className={classes.date} href="https://github.com/mjuncobranco" target="blank">
+        View my GitHub Portfolio.{" "}
+      </a>
+    </footer>
+  );
+}
+
+export default Footer;
